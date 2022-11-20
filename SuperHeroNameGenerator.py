f04bb49e4fcd154f49721276969a7984b8f4f7ed
Goura Koti <GouraKoti@users.noreply.github.com>
2022-11-20 21:37:07 +0000

    Create SuperHeroNameGenerator.py

---
+++ SuperHeroNameGenerator.py
@@ -0,0 +1,7 @@
+#In this project, I'm creating a neural network and train it on a small dataset of superhero names to learn to generate similar names. 
+#The dataset has over 9000 names of superheroes, supervillains and other fictional characters from a number of different comic books, TV shows and movies.
+#Text generation is a common natural language processing task. 
+#The idea is to create a character level language model that will predict the next character for a given input sequence. 
+#In order to get a new predicted superhero name, we need to give our model a seed input - this can be a single character or a sequence of characters, 
+#and the model will then generate the next character that it predicts should after the input sequence. 
+#This character is then added to the seed input to create a new input, which is then used again to generate the next character, and so on.
